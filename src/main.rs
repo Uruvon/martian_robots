c42e0efe6915cb3833366fc7ba31d6810f8b5de6
nicolas-slack <nicks@naurt.com>
2022-07-10 19:58:55 +0100

    doc: more documentation for main

--- src/main.rs
+++ src/main.rs
@@ -20,14 +20,27 @@ mod utils;
 
 // In future, these operations could pre programmed to have an opcode & opeffect, like this:
 // { op-code: 'L', op-mutation: '-90', op-type: 'rotation' } for an example rotation,
-// { op-code: 'F', op-mutation: '1', op-type: 'translation' } for an example translation.
+// { op-code: 'F', op-mutation: '+1', op-type: 'translation' } for an example translation.
 
-// These operations could be parsed by a small parser written by the engineer; allowing 
+// These operations could be parsed by a small parser written by the engineer; allowing
 // future expansions of the control system without needing to recompile each time a change
 // is made to the control structure of the robot.
 const VALID_COMMANDS: [char; 3] = ['L', 'R', 'F'];
 const VALID_DIRECTIONS: [char; 4] = ['N', 'E', 'S', 'W'];
 
+// Given more Time, I'd like to have made documentation & examples for this code base more thorough.
+// Additionally, i'd like to have allowed a co-ordinate system starting in a central manner, with
+// co-ordinates permissible between -width/2 & width/2, likewise with height.
+// As i would assume the mars doesn't always have to start in the bottom left corner.
+
+// Given more time a more robust set of test cases should be made, with some much longer
+// combinations of robots to test all boundary cases.
+
+// Additionally, the removal of 'expect' throughout the codebase, and substitution with more advanced
+// logging & metric would be ideal.
+
+// To-String methods for each of the custom structs would have been a nice addition, 
+// to improve the logging experience.
 #[instrument]
 fn main() {
     tracing_subscriber::fmt::init();
