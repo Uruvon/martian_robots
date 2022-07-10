27b3801d3a3399888334c8207ed8bce4a3901199
nicolas-slack <nicks@naurt.com>
2022-07-10 19:54:49 +0100

    doc: added some comments on control

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,19 @@ mod mars;
 mod robot;
 mod utils;
 
+// These constants can be extended to allow for additional commands at a later stage
+
+// If i were to have more time, i would load both these constants from configuration files
+// Allowing expansions without needed to recompile.
+// However, since the logic for handling these operations is currently hard coded, a re-compile would be needed anyway.
+
+// In future, these operations could pre programmed to have an opcode & opeffect, like this:
+// { op-code: 'L', op-mutation: '-90', op-type: 'rotation' } for an example rotation,
+// { op-code: 'F', op-mutation: '1', op-type: 'translation' } for an example translation.
+
+// These operations could be parsed by a small parser written by the engineer; allowing 
+// future expansions of the control system without needing to recompile each time a change
+// is made to the control structure of the robot.
 const VALID_COMMANDS: [char; 3] = ['L', 'R', 'F'];
 const VALID_DIRECTIONS: [char; 4] = ['N', 'E', 'S', 'W'];
 
