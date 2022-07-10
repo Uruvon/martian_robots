4c0f752ab919548334a2bdd4dff3d098c4425d93
nicolas-slack <nicks@naurt.com>
2022-07-10 17:32:26 +0100

    chore: refactored robot creation code - seperating concerns

--- src/robot.rs
+++ src/robot.rs
@@ -1,3 +1,8 @@
+use chrono::Utc;
+use tracing::{error, instrument};
+
+use crate::{VALID_DIRECTIONS, VALID_COMMANDS};
+
 /// A Struct representing a Martian exploratory robot.
 pub struct Robot {
     pub command_queue: Vec<char>,
@@ -15,11 +20,86 @@ pub struct Robot {
 }
 
 impl Robot {
-    pub fn new(coordinate: [usize; 2], command_queue: Vec<char>, direction: char) -> Self {
+    #[instrument]
+    pub fn new_from_lines(robot_seed: &str, robot_commands: &str) -> Self {
+        // TODO: Regex would be much more robust here; but given the requirements explicitly state
+        // Whitespace input separation, i'll use this for now.
+        let robot_seed_split: Vec<&str> = robot_seed.split(" ").collect();
+
+        let x_coord = usize::from_str_radix(robot_seed_split[0], 10).unwrap_or_else(|e| {
+            error!(
+                "{} | malformed input, robot x coord not a valid integer | {:?}",
+                Utc::now(),
+                e
+            );
+            panic!();
+        });
+
+        let y_coord = usize::from_str_radix(robot_seed_split[1], 10).unwrap_or_else(|e| {
+            error!(
+                "{} | malformed input, robot y coord  not a valid integer | {:?}",
+                Utc::now(),
+                e
+            );
+            panic!();
+        });
+
+        let direction_str = robot_seed_split[2];
+        if direction_str.len() > 1 || direction_str.len() == 0 {
+            error!(
+                "{} | malformed input, robot direction not a valid direction | {:?}",
+                Utc::now(),
+                direction_str
+            );
+            panic!();
+        }
+
+        let direction: char = direction_str.chars().next().unwrap_or_else(|| {
+            error!(
+                "{} | malformed input, robot direction  not a valid direction | {:?}",
+                Utc::now(),
+                direction_str
+            );
+            panic!();
+        });
+
+        if !VALID_DIRECTIONS.contains(&direction) {
+            error!(
+                "{} | malformed input, robot direction not a valid direction | {:?}",
+                Utc::now(),
+                direction
+            );
+            panic!();
+        }
+
+        let invalid_command_chars: Vec<char> = robot_commands
+            .chars()
+            .filter(|c| {
+                return !VALID_COMMANDS.contains(c);
+            })
+            .collect();
+        if !invalid_command_chars.len() == 0 {
+            error!(
+                "{} | malformed input, robot commands contains invalid command characters | {:?}",
+                Utc::now(),
+                invalid_command_chars
+            );
+            panic!();
+        }
+
+        let command_queue: Vec<char> = robot_commands.chars().collect();
+        if command_queue.len() > 100 || command_queue.len() == 0 {
+            error!(
+                "{} | malformed input, robot command queue is an invalid length| {:?}",
+                Utc::now(),
+                command_queue.len()
+            );
+            panic!();
+        }
         return Robot {
+            coordinate: [x_coord, y_coord],
             command_queue,
-            coordinate,
-            direction
+            direction,
         };
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,9 @@
-use std::fs;
 use chrono::Utc;
+use std::fs;
 
-use tracing::{instrument, error};
+use tracing::{error, instrument};
 
-use crate::{mars::Mars, VALID_DIRECTIONS, robot::Robot, VALID_COMMANDS};
+use crate::{mars::Mars, robot::Robot};
 
 /**
 Create a Martian Grid; with a maximum side length of 50.
@@ -12,13 +12,13 @@ Default Character is 'O', and empty,&& unexplored space
 */
 #[instrument]
 pub fn make_grid(y_size: usize, x_size: usize) -> Vec<Vec<char>> {
-	if x_size == 0 {
-		panic!("Cannot make a grid with no X size!")
-	}
+    if x_size == 0 {
+        panic!("Cannot make a grid with no X size!")
+    }
 
-	if y_size == 0 {
-		panic!("Cannot make a grid with no Y size!")
-	}
+    if y_size == 0 {
+        panic!("Cannot make a grid with no Y size!")
+    }
 
     let y = if y_size > 50 { 50 } else { y_size };
     let x = if x_size > 50 { 50 } else { x_size };
@@ -28,93 +28,60 @@ pub fn make_grid(y_size: usize, x_size: usize) -> Vec<Vec<char>> {
 
 #[instrument]
 pub fn load_simulation_file(filename: &'static str) -> Mars {
-	match fs::read_to_string(filename) {
-		Ok(contents) => {
-			let lines: Vec<&str> = contents.split("\n").collect();
+    match fs::read_to_string(filename) {
+        Ok(contents) => {
+            let lines: Vec<&str> = contents.split("\n").collect();
 
-			if lines.len() < 3 {
-				error!("{} | malformed input, less than 3 lines - may not have a single robot or world configuration", Utc::now());
-				panic!();
-			}
+            if lines.len() < 3 {
+                error!("{} | malformed input, less than 3 lines - may not have a single robot or world configuration", Utc::now());
+                panic!();
+            }
 
-			// TODO: Regex would be much more robust here; but given the requirements explicitly state
-			// Whitespace input separation, i'll use this for now.
-			let world_configuration: Vec<&str> = lines[0].split(" ").collect();
-			if world_configuration.len() > 2 {
-				error!("{} | malformed input, world configuration has more than two input groups", Utc::now());
-				panic!();
-			}
+            // TODO: Regex would be much more robust here; but given the requirements explicitly state
+            // Whitespace input separation, i'll use this for now.
+            let world_configuration: Vec<&str> = lines[0].split(" ").collect();
+            if world_configuration.len() > 2 {
+                error!(
+                    "{} | malformed input, world configuration has more than two input groups",
+                    Utc::now()
+                );
+                panic!();
+            }
 
-			let x_size = usize::from_str_radix(world_configuration[0], 10).unwrap_or_else(|e| {
-				error!("{} | malformed input, world configuration x not a valid integer | {:?}", Utc::now(), e);
-				panic!();
-			});
-			let y_size = usize::from_str_radix(world_configuration[1], 10).unwrap_or_else(|e| {
-				error!("{} | malformed input, world configuration y not a valid integer | {:?}", Utc::now(), e);
-				panic!();
-			});
-			
-			let mut mars = Mars::new(y_size, x_size);
+            let x_size = usize::from_str_radix(world_configuration[0], 10).unwrap_or_else(|e| {
+                error!(
+                    "{} | malformed input, world configuration x not a valid integer | {:?}",
+                    Utc::now(),
+                    e
+                );
+                panic!();
+            });
+            let y_size = usize::from_str_radix(world_configuration[1], 10).unwrap_or_else(|e| {
+                error!(
+                    "{} | malformed input, world configuration y not a valid integer | {:?}",
+                    Utc::now(),
+                    e
+                );
+                panic!();
+            });
 
-			for idx in (1..lines.len()).step_by(2) {
-				let robot_seed = lines[idx];
-				let robot_commands = lines[idx + 1];
+            let mut mars = Mars::new(y_size, x_size);
 
-				// TODO: Regex would be much more robust here; but given the requirements explicitly state
-				// Whitespace input separation, i'll use this for now.
-				let robot_seed_split: Vec<&str> = robot_seed.split(" ").collect();
+            for idx in (1..lines.len()).step_by(2) {
+                mars.robots
+                    .push(Robot::new_from_lines(lines[idx], lines[idx + 1]));
+            }
 
-				let x_coord = usize::from_str_radix(robot_seed_split[0], 10).unwrap_or_else(|e| {
-					error!("{} | malformed input, robot x coord not a valid integer | {:?}", Utc::now(), e);
-					panic!();
-				});
-
-				let y_coord = usize::from_str_radix(robot_seed_split[1], 10).unwrap_or_else(|e| {
-					error!("{} | malformed input, robot y coord  not a valid integer | {:?}", Utc::now(), e);
-					panic!();
-				});
-
-				let direction_str = robot_seed_split[2];
-				if direction_str.len() > 1 || direction_str.len() == 0 {
-					error!("{} | malformed input, robot direction not a valid direction | {:?}", Utc::now(), direction_str);
-					panic!();
-				}
-
-				let direction: char = direction_str.chars().next().unwrap_or_else(|| {
-					error!("{} | malformed input, robot direction  not a valid direction | {:?}", Utc::now(), direction_str);
-					panic!();
-				});
-
-				if !VALID_DIRECTIONS.contains(&direction) {
-					error!("{} | malformed input, robot direction not a valid direction | {:?}", Utc::now(), direction);
-					panic!();
-				}
-
-				let invalid_command_chars: Vec<char> = robot_commands.chars().filter(|c| { return !VALID_COMMANDS.contains(c); }).collect();
-				if !invalid_command_chars.len() == 0 {
-					error!("{} | malformed input, robot commands contains invalid command characters | {:?}", Utc::now(), invalid_command_chars);
-					panic!();
-				}
-
-				let command_queue: Vec<char> = robot_commands.chars().collect();
-				if command_queue.len() > 100 || command_queue.len() == 0 {
-					error!("{} | malformed input, robot command queue is an invalid length| {:?}", Utc::now(), command_queue.len());
-					panic!();
-				}
-				let robot = Robot::new(
-					[x_coord, y_coord],
-					command_queue,
-					direction
-				);
-
-				mars.robots.push(robot);
-			}
-
-			return mars
-		},
-		Err(e) => { 
-			error!("{} | could not load file contents | {:?} | {:?}", Utc::now(), filename, e);
-			panic!();
-		}
-	}
+            return mars;
+        }
+        Err(e) => {
+            error!(
+                "{} | could not load file contents | {:?} | {:?}",
+                Utc::now(),
+                filename,
+                e
+            );
+            panic!();
+        }
+    }
 }
