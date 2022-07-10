4264e1700ef5ccea129eb2fc4887d50e07a75fec
nicolas-slack <nicks@naurt.com>
2022-07-10 16:57:55 +0100

    chore: begun fleshing out unit tests for load_simulation

--- src/test/test_utils.rs
+++ src/test/test_utils.rs
@@ -1,4 +1,4 @@
-use crate::utils::make_grid;
+use crate::utils::{make_grid, load_simulation_file};
 
 #[test]
 pub fn test_make_grid_square() {
@@ -30,4 +30,40 @@ pub fn test_make_grid_oversized_x() {
   assert_eq!(side_25.len(), 25);
   assert_eq!(side_25[0].len(), 50);
   assert_eq!(side_25[0][5], 'O');
+}
+
+
+
+
+
+
+#[test]
+pub fn test_load_simulation_simple() {
+  let mars = load_simulation_file("resources/test/test_input_simple.txt");
+  assert!(mars.planetary_grid.len() == 3);
+  assert!(mars.planetary_grid[0].len() == 5);
+}
+
+#[test]
+#[should_panic]
+pub fn test_load_simulation_too_short() {
+  let mars = load_simulation_file("resources/test/test_input_short.txt");
+}
+
+#[test]
+#[should_panic]
+pub fn test_load_simulation_bad_world() {
+  let mars = load_simulation_file("resources/test/test_input_bad_world.txt");
+}
+
+#[test]
+#[should_panic]
+pub fn test_load_simulation_bad_world_x() {
+  let mars = load_simulation_file("resources/test/test_input_bad_world_x.txt");
+}
+
+#[test]
+#[should_panic]
+pub fn test_load_simulation_bad_world_y() {
+  let mars = load_simulation_file("resources/test/test_input_bad_world_y.txt");
 }
